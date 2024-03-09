741b211db2a8c75102a7eafae4436fea9ee3a92b
johnc <johncunan07@gmail.com>
2024-03-09 20:41:21 +0800

    completed project

--- src/main/java/dev/john/movies/MovieController.java
+++ src/main/java/dev/john/movies/MovieController.java
@@ -1,13 +1,16 @@
 package dev.john.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -18,4 +21,19 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
+
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String id) {
+//        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
+//    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovieByImdbId (@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+
+//    @GetMapping("/{title}")
+//    public ResponseEntity<Optional<Movie>> getSingleMovieByTitle (@PathVariable String title) {
+//        return new ResponseEntity<Optional<Movie>>(movieService.singleMovieByTitle(title), HttpStatus.OK);
+//    }
 }
--- src/main/java/dev/john/movies/MovieService.java
+++ src/main/java/dev/john/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.john.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,4 +14,15 @@ public class MovieService {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+//    public Optional<Movie> singleMovie(ObjectId id) {
+//        return movieRepository.findById(id);
+//    }
+    public Optional<Movie> singleMovieByImdbId(String imdbId) {
+            return movieRepository.findMovieByImdbId(imdbId);
+    }
+
+//    public Optional<Movie> singleMovieByTitle(String title) {
+//        return movieRepository.findMovieByTitle(title);
+//    }
 }
